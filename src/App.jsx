4a607bbc92ae6bccb7afe0f91a88656aa290df98
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-21 21:41:20 +0500

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import carsData from '../db.json'
 const App = () => {
-  console.log(carsData)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      hello
+    </>
   )
 }
 
