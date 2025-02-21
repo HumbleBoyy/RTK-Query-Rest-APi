fdd0e6bddded30ff701caa83f38ba938fbbc2065
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-21 22:41:41 +0500

    New Commit

---
+++ src/store/carsApi.jsx
@@ -0,0 +1,14 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const carsApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/carsData' }),
+  endpoints: (builder) => ({
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPokemonByNameQuery } = carsApi
